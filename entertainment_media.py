493b558dcaa2135177110ca1b4547710e2d0e65e
Mahesh Kumar <maheshkumar.wdp21@gmail.com>
2016-04-20 23:17:32 +0530

    Improved the code

--- entertainment_media.py
+++ entertainment_media.py
@@ -21,15 +21,44 @@ def get_movie_title(movie_name):
 	return movie_title
 
 # Instances of the Class Movie
-toy_story = media.Movie("Toy Story", get_movie_title('Toy Story'), get_movie_poster('Toy Story'), "https://www.youtube.com/watch?v=vwyZH85NQC4")
-avatar = media.Movie("Avatar", get_movie_title('Avatar'), get_movie_poster('Avatar'), "https://www.youtube.com/watch?v=5PSNL1qE6VY")
-extra_terrestrial = media.Movie("E.T", get_movie_title('Extra Terrestrial'), get_movie_poster('Extra Terrestrial'), "https://www.youtube.com/watch?v=oR1-UFrcZ0k")
-harry_potter = media.Movie("Harry Potter", get_movie_title('Harry Potter'), get_movie_poster('Harry Potter'), "https://www.youtube.com/watch?v=PbdM1db3JbY")
-indian_jones = media.Movie("Indiana Jones", get_movie_title('Indiana Jones'), get_movie_poster('Indiana Jones'), "https://www.youtube.com/watch?v=nMhfESAa4tw")
-deadpool = media.Movie("Deadpool", get_movie_title('Deadpool'), get_movie_poster('Deadpool'), "https://www.youtube.com/watch?v=ZIM1HydF9UA")
+toy_story = media.Movie(
+	"Toy Story", 
+	get_movie_title('Toy Story'), 
+	get_movie_poster('Toy Story'), 
+	"https://www.youtube.com/watch?v=vwyZH85NQC4")
+
+avatar = media.Movie(
+	"Avatar", 
+	get_movie_title('Avatar'), 
+	get_movie_poster('Avatar'), 
+	"https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+extra_terrestrial = media.Movie(
+	"E.T", 
+	get_movie_title('Extra Terrestrial'), 
+	get_movie_poster('Extra Terrestrial'), 
+	"https://www.youtube.com/watch?v=oR1-UFrcZ0k")
+
+harry_potter = media.Movie(
+	"Harry Potter", 
+	get_movie_title('Harry Potter'), 
+	get_movie_poster('Harry Potter'), 
+	"https://www.youtube.com/watch?v=PbdM1db3JbY")
+
+indiana_jones = media.Movie(
+	"Indiana Jones", 
+	get_movie_title('Indiana Jones'), 
+	get_movie_poster('Indiana Jones'), 
+	"https://www.youtube.com/watch?v=nMhfESAa4tw")
+
+deadpool = media.Movie(
+	"Deadpool", 
+	get_movie_title('Deadpool'), 
+	get_movie_poster('Deadpool'), 
+	"https://www.youtube.com/watch?v=ZIM1HydF9UA")
 
 # List of all the movies
-movies = [toy_story, avatar, extra_terrestrial, harry_potter, indian_jones, deadpool]
+movies = [toy_story, avatar, extra_terrestrial, harry_potter, indiana_jones, deadpool]
 
 # Passing the movies list to fresh_tomatoes
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -2,6 +2,10 @@ import webbrowser
 
 class Movie:
 	def __init__(self, movie_title, movie_story, poster_image, youtube_url):
+		"""
+			The init method get's initialized when an instance
+			of this class is created in any other class.
+		"""
 		self.title = movie_title
 		self.storyline = movie_story
 		self.poster_image_url = poster_image
@@ -9,4 +13,9 @@ class Movie:
 
 	# Function to show trailer for a particular movie
 	def show_trailer(self):
+		"""
+			The show_trailer instance method will 
+			show the trailer of the movie, when the 
+			movie poster is clicked.
+		"""
 		webbrowser.open(self.url)
